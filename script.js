c004bfd4730d18d1cfcda87bee95a59c653675e3
ehb2004 <149546842+ehb2004@users.noreply.github.com>
2025-02-25 15:09:34 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var el = (document.getElementById("body").style.color = "pink");
+//Taken from Dr.Cumbie's github.
+$(document).ready(function () {
+  console.log("ready!");
+});
+
+function milesPerHour(dist, time) {
+  return dist / (time / 60);
+}
+console.log(milesPerHour(2, 30));
+document.body.style.backgroundColor = "F8BBD0";
+
+var el = document.getElementById("firstLog");
+<script>
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
